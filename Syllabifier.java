ae4b40a83888cad99258a8d55d1674d00d62ddf4
Bonnie Chen <boc016@ucsd.edu>
2018-03-02 22:27:17 -0800

    clean up code and implement reading from file

--- Syllabifier.java
+++ Syllabifier.java
@@ -2,51 +2,57 @@
  * Filename: Syllabifier.java TODO: better name?
  * Author: archelonian@github
  * Date of creation: 3/1/2018
- * Description: Partitions a string over {C, V}* into acceptable syllables based
+ * Description: Partitions strings over {C, V}* into acceptable syllables based
  *              on a supplied list of permissible syllable formats (e.g. CV,
  *              CVC, CCVC).
  */
 
-// disclaimer: yup I switched to java. actually I should probably switch again
-//             to javascript, but parsing java to javascript should either be
-//             easy or something to practice with.
-
 import java.util.* ;
+import java.io.* ;
 
 public class Syllabifier{
+   public static final int EXPECTED_ARGS = 2 ;
+
    // words to partition
    private ArrayList<String> words ;
    // permitted syllable patterns TODO: select better data structure? consider
-   //    using a hash map or stream instead of ArrayList
+   //    using a hash map or stream (???) instead of ArrayList
    private ArrayList<String> patterns ;
 
    // the key is the word and the value is all found partitions for the word
-   //    (e.g. "CV|CV") 
-   private HashMap<String, ArrayList<String>> outputs ;
+   private HashMap<String, ArrayList<String>> partitions ;
 
    // constructor
-   public Syllabifier(){
-      // TODO: read from file, do not permit empty strings
+   public Syllabifier(String wordsPath, String patternsPath)
+      throws FileNotFoundException{
 
-      // dummy values to test with
-      // String[] dummyWords = {"CVC", "CVCCVCVC", "CVCVCCCVVC", "CVCV", "CVCCV"} ;
-      String[] dummyWords = {"CVCCV"} ;
-      String[] dummyPatterns = {"CV", "CVC"} ;
+      // read from file
+      Scanner scanWords = new Scanner(new File(wordsPath)) ;
+      Scanner scanPatterns = new Scanner(new File(patternsPath)) ;
+
+      String newWord ;
+      String newPattern ;
 
       // populate instance variables
       words = new ArrayList<String>() ;
       patterns = new ArrayList<String>() ;
-      outputs = new HashMap<String, ArrayList<String>>() ;
+      partitions = new HashMap<String, ArrayList<String>>() ;
 
-      for(String word : dummyWords){
-         words.add(word) ;
+      // happily, the scanner does not read blank lines
+      while(scanWords.hasNextLine()){
+         newWord = scanWords.nextLine() ;
 
+         words.add(newWord) ;
+
+         // have an empty list ready for each word
          ArrayList<String> emptyList = new ArrayList<String>() ;
-         outputs.put(word, emptyList) ;
+         partitions.put(newWord, emptyList) ;
       }
 
-      for(String pattern : dummyPatterns){
-         patterns.add(pattern) ;
+      while(scanPatterns.hasNextLine()){
+         newPattern = scanPatterns.nextLine() ;
+
+         patterns.add(newPattern) ;
       }
    } // end constructor
 
@@ -55,9 +61,6 @@ public class Syllabifier{
    private void partition(){
       // inspect each word
       for(String word : words){
-         // TODO: debug code, delete later
-         System.out.println("in word: " + word) ;
-
          // get syllable splits, start by checking from the first character and
          //    the accumulator starts empty
          if(word.length() > 1){
@@ -66,7 +69,7 @@ public class Syllabifier{
          else {
             // only one character, add to partitions
             if(patterns.contains(word)){
-               outputs.get(word).add(word) ;
+               partitions.get(word).add(word) ;
             }
          }
       }
@@ -79,27 +82,13 @@ public class Syllabifier{
       }
    }
 
-   // TODO: debug code, delete later
-   private void printOutputs(){
-      System.out.println("printing hashmap outputs") ;
-      for(String word : words){
-         System.out.println("word: " + word) ;
-         if(outputs.get(word) == null){
-            System.out.println("null") ;
-         }
-         else{
-            printList(outputs.get(word)) ;
-         }
-      }
-   }
-
    // checkSyll recursively checks if it is able to complete a valid syllable
-   //    and move on. The goal here is to recurse on the base itself, then the
-   //    base and one more character, then two, until no matching pattern can
-   //    be found and that path is deemed impossible to further partition.
+   //    and move on. The goal here is to recurse on the first character, then
+   //    the first two characters, and so on until no matching patterns can be
+   //    found and that path is deemed impossible to further partition.
    //
    // segment is the part to recurse over and acc is previously processed valid
-   //    syllables in the word
+   //    syllables in the word. original is included to index into partitions
    private void checkSyll(String original, String segment, String acc){
       String currUnit ;
       String newAcc = "" ;
@@ -108,45 +97,27 @@ public class Syllabifier{
       for(int prefix = 1; prefix <= segment.length(); prefix++){
          currUnit = segment.substring(0, prefix) ;
 
-         // TODO: debug code, delete later
-         System.out.println("looking for " + currUnit) ;
-
          if(patterns.contains(currUnit)){
             // this is a valid syllable, put into newAcc as a syllable found in
             //    this iteration only
             newAcc = currUnit ;
-
-            // TODO: debug code, delete later
-            System.out.println("found " + currUnit) ;
 
             // recurse as long as currUnit is not the entire segment
             if(prefix < segment.length()){
                // add a boundary because there will be more characters
                newAcc += "|" ;
 
-               // TODO: debug code, delete later
-               System.out.println("not end, adding " + currUnit + " and |") ;
-
                // recurse on the rest of the segment, combine top level
                //    accumulator with newly found syllable
-               // TODO: debug code, delete later
-               System.out.println("recursing on " + segment.substring(prefix)) ;
                checkSyll(original, segment.substring(prefix), acc + newAcc) ;
             }
             else {
                // if currUnit is the entire segment and it is a valid syllable,
                //    partition found
-               outputs.get(original).add(acc + currUnit) ;
-
-               // TODO: debug code, delete later
-               System.out.println("adding " + acc + currUnit) ;
+               partitions.get(original).add(acc + currUnit) ;
             }
          }
-         else {
-            // TODO: debug code, delete later
-            System.out.println("did not find " + currUnit) ;
-         }
-      }
+      } // end for loop
    } // end checkSyll
 
    // perform program output: write to file
@@ -156,6 +127,16 @@ public class Syllabifier{
       ArrayList<String> currPartitions ;
       String currWord ;
 
+      // remind reader what the syllables were
+      System.out.print("Permitted syllables: ") ;
+
+      for(int i = 0; i < patterns.size(); i++){
+         System.out.print(patterns.get(i) + " ") ;
+      }
+
+      // spacing
+      System.out.println() ;
+
       // iterate over the words in sequence
       for(int i = 0; i < words.size(); i++){
          // spacing
@@ -164,24 +145,32 @@ public class Syllabifier{
          currWord = words.get(i) ;
 
          System.out.println("Printing partitions for " + currWord) ;
-         // get an array of all the partitions associated with this word
-         currPartitions = outputs.get(currWord) ;
+         // get the list of all the partitions associated with this word
+         currPartitions = partitions.get(currWord) ;
 
          if(currPartitions.isEmpty()){
-            System.out.println("\tNo valid partitions found for word "
-                               + currWord) ;
+            System.out.println("   No valid partitions found for " + currWord) ;
          }
 
          // print values in sequence
          for(int j = 0; j < currPartitions.size(); j++){
-            System.out.println("\t" + currPartitions.get(j)) ;
+            System.out.println("   " + currPartitions.get(j)) ;
          }
       }
    } // end output
 
-   // main methods, creates a Syllabifier object and runs its methods
-   public static void main(String[] args){
-      Syllabifier sy = new Syllabifier() ;
+   // main method, creates a Syllabifier object and runs its methods
+   public static void main(String[] args) throws FileNotFoundException{
+      if(args.length != EXPECTED_ARGS){
+         System.out.println("usage: java Syllabifier [words filepath] "
+                            + "[syllables filepath]") ;
+         System.out.println("\nwords and syllables files should have one word "
+                            + "or syllable per line") ;
+         return ;
+      }
+
+      // construct a Syllabifier and initialize its variables
+      Syllabifier sy = new Syllabifier(args[0], args[1]) ;
 
       sy.partition() ;
       sy.output() ;
