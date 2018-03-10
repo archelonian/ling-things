1ea49492bf0d6d04156b40ec99713b3e6e0cb8f4
Bonnie Chen <boc016@ucsd.edu>
2018-03-10 00:54:07 -0800

    implement rudimentary, buggy weighting

--- Syllabifier.java
+++ Syllabifier.java
@@ -12,16 +12,22 @@ import java.io.* ;
 
 public class Syllabifier{
    public static final int EXPECTED_ARGS = 2 ;
-   public static final String BOUNDARY = "|" ;
+   public static final String BOUNDARY = "." ;
 
    // words to partition
    private ArrayList<String> words ;
    // permitted syllable patterns TODO: select better data structure? consider
    //    using a hash map or stream (???) instead of ArrayList
-   private ArrayList<String> patterns ;
+   // private ArrayList<String> patterns ;
+   // permitted syllable patterns where the key is the pattern and the value is
+   //    its relative weight
+   private HashMap<String, Integer> patterns ;
 
    // the key is the word and the value is all found partitions for the word
    private HashMap<String, ArrayList<String>> partitions ;
+
+   // TODO: improve this structure if possible
+   private HashMap<String, Integer> partitionValues ;
 
    // constructor
    public Syllabifier(String wordsPath, String patternsPath)
@@ -36,8 +42,10 @@ public class Syllabifier{
 
       // populate instance variables
       words = new ArrayList<String>() ;
-      patterns = new ArrayList<String>() ;
+      // patterns = new ArrayList<String>() ;
+      patterns = new HashMap<String, Integer>() ;
       partitions = new HashMap<String, ArrayList<String>>() ;
+      partitionValues = new HashMap<String, Integer>() ;
 
       while(scanWords.hasNextLine()){
          newWord = scanWords.nextLine() ;
@@ -50,9 +58,15 @@ public class Syllabifier{
       }
 
       while(scanPatterns.hasNextLine()){
-         newPattern = scanPatterns.nextLine() ;
+         int weight = 0 ;
 
-         patterns.add(newPattern) ;
+         newPattern = scanPatterns.nextLine() ;
+         String parts[] = newPattern.split(" ") ;
+         // TODO: error checking. what if the user forgets the weight?
+         weight = Integer.parseInt(parts[1]) ;
+
+         patterns.put(parts[0], weight) ;
+         // patterns.add(newPattern) ;
       }
    } // end constructor
 
@@ -62,14 +76,15 @@ public class Syllabifier{
       // inspect each word
       for(String word : words){
          // get syllable splits, start by checking from the first character and
-         //    the accumulator starts empty
+         //    the accumulator and total weight start empty
          if(word.length() > 1){
-            checkSyll(word, word, "") ;
+            checkSyll(word, word, "", 0) ;
          }
          else {
             // only one character, add to partitions
-            if(patterns.contains(word)){
+            if(patterns.containsKey(word)){
                partitions.get(word).add(word) ;
+               partitionValues.putIfAbsent(word, patterns.get(word)) ;
             }
          }
       }
@@ -88,19 +103,22 @@ public class Syllabifier{
    //    found and that path is deemed impossible to further partition.
    //
    // segment is the part to recurse over and acc is previously processed valid
-   //    syllables in the word. original is included to index into partitions
-   private void checkSyll(String original, String segment, String acc){
+   //    syllables in the word. original is included only to index into
+   //    partitions and val is the weighted value of this partitioning
+   private void checkSyll(String original, String segment, String acc, int val){
       String currUnit ;
       String newAcc = "" ;
+      int newVal = 0 ;
 
       // from first character only to entire segment
       for(int prefix = 1; prefix <= segment.length(); prefix++){
          currUnit = segment.substring(0, prefix) ;
 
-         if(patterns.contains(currUnit)){
+         if(patterns.containsKey(currUnit)){
             // this is a valid syllable, put into newAcc as a syllable found in
-            //    this iteration only
+            //    this iteration only and add its value
             newAcc = currUnit ;
+            newVal += patterns.get(currUnit) ;
 
             // recurse as long as currUnit is not the entire segment
             if(prefix < segment.length()){
@@ -109,12 +127,14 @@ public class Syllabifier{
 
                // recurse on the rest of the segment, combine top level
                //    accumulator with newly found syllable
-               checkSyll(original, segment.substring(prefix), acc + newAcc) ;
+               checkSyll(original, segment.substring(prefix), acc + newAcc,
+                         val + newVal) ;
             }
             else {
                // if currUnit is the entire segment and it is a valid syllable,
                //    partition found
                partitions.get(original).add(acc + currUnit) ;
+               partitionValues.putIfAbsent((acc + currUnit), val) ;
             }
          }
       } // end for loop
@@ -124,12 +144,18 @@ public class Syllabifier{
    private void output(){
       ArrayList<String> currPartitions ;
       String currWord ;
+      Set<String> keys = patterns.keySet() ;
+      Iterator<String> it = keys.iterator() ;
+      String currPartition = "" ;
 
       // remind reader what the syllables were
       System.out.print("Permitted syllables: ") ;
 
-      for(int i = 0; i < patterns.size(); i++){
-         System.out.print(patterns.get(i) + " ") ;
+      // for(int i = 0; i < patterns.size(); i++){
+      //    System.out.print(patterns.get(i) + " ") ;
+      // }
+      while(it.hasNext()){
+         System.out.print(it.next() + " ") ;
       }
 
       // spacing
@@ -152,10 +178,17 @@ public class Syllabifier{
 
          // print values in sequence
          for(int j = 0; j < currPartitions.size(); j++){
-            System.out.println("   " + currPartitions.get(j)) ;
+            currPartition = currPartitions.get(j) ;
+            System.out.print("   " + currPartition) ;
+            System.out.println(" " + partitionValues.get(currPartition)) ;
          }
       }
    } // end output
+
+   // getter method for the found partitions
+   public HashMap<String, ArrayList<String>> getPartitions(){
+      return partitions ;
+   }
 
    // main method, creates a Syllabifier object and runs its methods
    public static void main(String[] args) throws FileNotFoundException{
